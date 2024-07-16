84346f2f1a51f43281bcbe5cc05b79b880fe830c
Achyut Pal <achyut21pal@gmail.com>
2024-07-16 21:00:45 +0530

    Initial Commit - Sampark Khata

---
+++ src/main/java/com/samparkkhata/SamparkKhataApplication.java
@@ -0,0 +1,13 @@
+package com.samparkkhata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamparkKhataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamparkKhataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samparkkhata/SamparkKhataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samparkkhata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamparkKhataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
